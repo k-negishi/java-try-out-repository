f07fe1a2a32ae871eac8b5ee48737445a58a661c
KeiNegishi <56011598+keinegishi@users.noreply.github>
2022-10-22 17:35:21 +0900

    といてみた
    【新卒・中途採用】サーバーサイドエンジニア応募者向けの模試 | ゆめみ
    https://www.yumemi.co.jp/serverside_recruit

---
+++ yumemi-problem/src/GetRanking.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+/**
+ * https://www.yumemi.co.jp/serverside_recruit
+ * 【新卒・中途採用】サーバーサイドエンジニア応募者向けの模試を解いてみる
+ *
+ * game_score_log.csvから上位3位を出力するためのクラス
+ *
+ * 実装速度優先のため、例外処理は未実装
+ */
+public class GetRanking {
+    public static final int DEFAULT_PLAY_COUNT = 1;
+
+    public static void main(String[] args) throws IOException {
+        Path rankingPath = Path.of("yumemi-problem/game_score_log.csv");
+
+        // System.out.println(Files.readString(rankingPath));
+
+        List<PlayerScore> playerScoreList = new ArrayList<>();
+        // key:player id, value:listのindexを管理すためのMap
+        Map<String, Integer> playerIdMap = new HashMap<>();
+
+        //        Set<String> playerIdSet = new HashSet<>();
+
+        BufferedReader reader = Files.newBufferedReader(rankingPath);
+
+        // csvファイルを1行ずつ読み込む https://medium-company.com/java-csv-%E8%AA%AD%E3%81%BF%E8%BE%BC%E3%81%BF/
+        // 1行ずつ読み込むことで、大量データにも対応した
+        int rowIndex = 0;
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // csvの1行目はスキップ
+            if (rowIndex == 0) {
+                rowIndex++;
+                continue;
+            }
+
+            // 行の読み込み
+            String[] data = line.split(",");
+            String playerId = data[1];
+            int score = Integer.parseInt(data[2]);
+
+            // すでに集計されたplayerIdの存在有無で処理を分岐
+
+            /* ここから残骸
+            if (!playerMap.containsKey(playerId)) {
+                playerMap.put(playerId, score);
+
+            } else {
+                Integer nowScore = (Integer) playerMap.get(playerId);
+                Integer meanScore =
+
+            }
+             */
+
+            if (!playerIdMap.containsKey(playerId)) {
+                int mapIndex = playerIdMap.size();
+                playerIdMap.put(playerId, mapIndex);
+                playerScoreList.add(new PlayerScore(playerId, score, DEFAULT_PLAY_COUNT));
+
+            } else {
+                int index = playerIdMap.get(playerId);
+                PlayerScore playerScore = playerScoreList.get(index);
+                int sumScore = playerScore.getScore() + score;
+                PlayerScore newRecord = new PlayerScore(playerScore.getPlayerId(), sumScore, playerScore.getPlayCount() + 1);
+
+                playerScoreList.set(index, newRecord);
+            }
+        }
+
+        // 平均を計算する
+        Map<String, Integer> averageScoreMap = new HashMap<>();
+        for (PlayerScore playerScore : playerScoreList) {
+            String playerId = playerScore.getPlayerId();
+            int averageScore = BigDecimal.valueOf(playerScore.getScore()).divide(BigDecimal.valueOf(playerScore.getPlayCount()), 1, RoundingMode.HALF_UP).intValue();
+
+            averageScoreMap.put(playerId, averageScore);
+        }
+
+        // スコアが高い順にソート http://kevin3sei.blog95.fc2.com/blog-entry-159.html
+        // TODO:もっといい方法があるはず
+        Map<String, Integer> sortedMap = averageScoreMap.entrySet().stream()
+                .sorted(Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        // forループに入れるためにList化。
+        // TODO:本当はStreamで上手くやりたいが...
+        List<Entry<String, Integer>> sortedScoreList = new ArrayList<>(sortedMap.entrySet());
+
+         // ランキング初期値
+        int ranking = 1;
+
+        // SCV形式のヘッダー出力
+        System.out.println("rank,player_id,mean_score");
+        // 上位3位までを出力するための処理
+        for (int i = 0; i < sortedScoreList.size(); i++) {
+            Entry<String, Integer> entry = sortedScoreList.get(i);
+            String playerId = entry.getKey();
+            int averageScore = entry.getValue();
+
+            // 前のレコードとスコアが異なる場合にランキングを加算
+            if (i !=0 && averageScore!= sortedScoreList.get(i-1).getValue()) {
+                ranking++;
+            }
+            // ランキング3位まででbreak
+            if (ranking > 3) break;
+
+            System.out.println(ranking + "," + playerId + "," + averageScore);
+        }
+
+
+    }
+
+}
+
+class PlayerScore {
+    private String playerId;
+    private int score;
+    private int playCount;
+
+    public PlayerScore(String playerId, int score, int playCount) {
+        this.playerId = playerId;
+        this.score = score;
+        this.playCount = playCount;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(int playCount) {
+        this.playCount = playCount;
+    }
+}
+
